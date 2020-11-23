d7b34859210f5ecd7b96824b4a45b01704e68fab
MoviezHood <73480087+MoviezHood@users.noreply.github.com>
2020-11-23 22:46:02 +0530

    Initial commit

---
+++ translation.py
@@ -0,0 +1,27 @@
+class Translation(object):
+    START_TEXT = """ A Simple File Renamer Bot With Permanent Thumbnail support!💯
+
+<b>Send me any Telegram file and reply to that file with /rename NewName.extn</b>"""
+
+    RENAME_403_ERR = "What Are You Doing? You are Banned"
+    ABS_TEXT = "What Are You Trying To Do,Mate?"
+    UPGRADE_TEXT = "CONTACT @prgofficial"
+    DOWNLOAD_START = "Give Me Some Time Bruh!!(〒﹏〒)"
+    UPLOAD_START = "Hurray... Completed Downloading(;ŏ﹏ŏ)"
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry WTF Do You Think! I'll Upload It?"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me > ©  @prgofficial **"
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
+    NOT_AUTH_USER_TEXT = "CONTACT @prgofficial"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @prgofficial"
+    SAVED_CUSTOM_THUMB_NAIL = "Thumbnail Saved ✅ This Is Permanent"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Custom thumbnail cleared succesfully🤦"
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
+    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    CUSTOM_CAPTION_UL_FILE = " "
+    NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found,SED🤒"
+    USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+    HELP_USER = """It's not that complicated😅
+    
+1. Send me any Telegram File.
+2. Reply to that message with /rename NEWNAME.EXTENSION"""
+
