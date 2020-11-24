acd64666ad61c9a1bfce3d0b10a343456d22efe9
PROFESSOR <66514907+Adithyan1133-ctrl@users.noreply.github.com>
2020-11-24 12:16:22 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -36,26 +36,22 @@ from database.database import *
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["rename"]))
 async def echo(bot, update):
-    
-    update_channel = Config.UPDATE_CHANNEL
-    if update_channel:
-        try:
-            user = await bot.get_chat_member(update_channel, update.chat.id)
-            if user.status == "kicked":
-               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
-               return
-        except UserNotParticipant:
-            #await update.reply_text(f"Join @{update_channel} To Use Me")
-            await update.reply_text(
-                text="**Join My Updates Channel to use ME 😎 🤭**",
-                reply_markup=InlineKeyboardMarkup([
-                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/{update_channel}")]
+    try:
+       user = await bot.get_chat_member("TGOpenBotz", update.chat.id)
+       if user.status == "kicked":
+          await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+          return
+    except UserNotParticipant:
+       await update.reply_text(
+             text="**Join My Updates Channel to use ME 😎 🤭**",
+             reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Join My Updates Channel", url=f"https://t.me/TGOpenBotz")]
               ])
-            )
-            return
-        except Exception:
-            await update.reply_text("Something Wrong. Contact my Support Group")
-            return
+       )
+       return
+    except Exception:
+       await update.reply_text("Something Wrong. Contact my Support Group")
+       return
     if update.from_user.id in Config.BANNED_USERS:
         await bot.delete_messages(
             chat_id=update.chat.id,
