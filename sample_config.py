b305696d9834531397a54f3f05b4d59f0f42ef0a
MoviezHood <73480087+MoviezHood@users.noreply.github.com>
2020-11-24 10:39:02 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -21,6 +21,8 @@ class Config(object):
     FREE_USER_MAX_FILE_SIZE = 50000000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
     # proxy for accessing youtube-dl in GeoRestricted Areas
