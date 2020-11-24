275d2ab4512004baca87b2b432b812ff4caa28da
MoviezHood <73480087+MoviezHood@users.noreply.github.com>
2020-11-24 11:22:32 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -66,7 +66,7 @@ async def echo(bot, update):
             revoke=True
         )
         return
-   TRChatBase(update.from_user.id, update.text, "rename")
+   """TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
         if len(file_name) > 64:
@@ -76,7 +76,7 @@ async def echo(bot, update):
                     num=len(file_name)
                 )
             )
-            return
+            return"""
 
     description = Translation.CUSTOM_CAPTION_UL_FILE
     download_location = Config.DOWNLOAD_LOCATION + "/"
